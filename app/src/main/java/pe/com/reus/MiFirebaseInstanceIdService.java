65ce81f82962dfd33d21f19db0cb319bbc1c2136
jorvil <jorvil_06@hotmail.com>
2018-12-03 22:14:20 -0500

    Actulizacion de maps y push 2

--- app/src/main/java/pe/com/reus/MiFirebaseInstanceIdService.java
+++ app/src/main/java/pe/com/reus/MiFirebaseInstanceIdService.java
@@ -1,19 +1,21 @@
 package pe.com.reus;
 
 import android.util.Log;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
-public class MiFirebaseInstanceIdService {//extends FirebaseInstanceIdService {
 
-    public static final String TAG="NOTICIAS";
+public class MiFirebaseInstanceIdService extends FirebaseMessagingService  {
 
-    /*
     @Override
-    public void onTokenRefresh() {
-        super.onTokenRefresh();
-
-        String token = FirebaseInstanceId.getInstance().getToken();
-
-        Log.d(TAG, "Token" + token);
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        Log.e("Refreshed token:",token);
     }
-    */
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+
 }
